d0a51a5de5b34c7101de6e042639211150c4d48a
x-huang19 <92767234+x-huang19@users.noreply.github.com>
2026-06-29 15:56:54 +0800

    Disable Uvicorn color auto-detection

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
+from __future__ import annotations
+
+import ctypes
+import os
+import sys
+
+
+def _ensure_standard_streams() -> None:
+    if sys.stdout is None:
+        sys.stdout = open(os.devnull, "w", encoding="utf-8")
+    if sys.stderr is None:
+        sys.stderr = open(os.devnull, "w", encoding="utf-8")
+
+
+def _hide_frozen_console() -> None:
+    if sys.platform != "win32" or not getattr(sys, "frozen", False):
+        return
+
+    console_window = ctypes.windll.kernel32.GetConsoleWindow()
+    if console_window:
+        ctypes.windll.user32.ShowWindow(console_window, 0)
+
+
+_ensure_standard_streams()
+_hide_frozen_console()
+
 from app.main import run
 
 
